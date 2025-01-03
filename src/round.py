d15cd49ee3c17e357039b3eb4ed51638ca7a5b1f
wojtekc135 <wojtekc135@gmail.com>
2025-01-04 00:28:20 +0100

    Add count_known_for_player method to Round class

--- src/round.py
+++ src/round.py
@@ -1,3 +1,10 @@
 class Round:
     def __init__(self, round_number):
         self.round_number = round_number
+
+    def count_known_for_player(self, hand):
+        sum = 0
+        for card in hand:
+            if card.known_for_player:
+                sum += 1
+        return sum
