99be112835a4097129a2dd4443a7b7a5cea707e4
wojtekc135 <wojtekc135@gmail.com>
2025-01-04 00:45:47 +0100

    Fix incorrect indentation in player.py

--- src/player.py
+++ src/player.py
@@ -3,9 +3,9 @@ class Player:
         self.isHuman = isHuman
         self.player_number = player_number
 
-        def swap_card(self, state, card1, card2):
-            state[card1.location][card1.location_number], state[card2.location][card2.location_number] = \
-                state[card2.location][card2.location_number], state[card1.location][card1.location_number]
-            card1.location, card2.location = card2.location, card1.location
-            card1.location_number, card2.location_number = card2.location_number, card1.location_number
-            return state, card1, card2
+    def swap_card(self, state, card1, card2):
+        state[card1.location][card1.location_number], state[card2.location][card2.location_number] = \
+            state[card2.location][card2.location_number], state[card1.location][card1.location_number]
+        card1.location, card2.location = card2.location, card1.location
+        card1.location_number, card2.location_number = card2.location_number, card1.location_number
+        return state, card1, card2
