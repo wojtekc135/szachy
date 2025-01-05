672b2c70ad8dcddc0c2c49bbd1ae447fa4ea7110
wojtekc135 <wojtekc135@gmail.com>
2025-01-05 05:55:34 +0100

    Add player methods to choose cards from hand and pile

--- src/player.py
+++ src/player.py
@@ -1,3 +1,5 @@
+from input_handler import InputHandler
+
 class Player:
     def __init__(self, isHuman, player_number):
         self.isHuman = isHuman
@@ -9,3 +11,16 @@ class Player:
         card1.location, card2.location = card2.location, card1.location
         card1.location_number, card2.location_number = card2.location_number, card1.location_number
         return state, card1, card2
+
+    def choose_stack_type(self,state):
+        card = InputHandler.choose_from(state["face_up_pile"] + state["face_down_pile"])
+        stack_type = card.location
+        return stack_type
+
+    def choose_card_from_stack(self,state, stack_type, index):
+        card = state[stack_type][index]
+        return card
+
+    def choose_card_from_hand(self,state, hand_name):
+        card = InputHandler.choose_from(state[hand_name])
+        return card
