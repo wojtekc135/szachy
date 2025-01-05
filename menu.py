a61fb5a77e688ef2da1b9f0cf33a328d0547d15d
brixen05 <majka.skora@gmail.com>
2025-01-05 22:01:35 +0100

    Dodanie do assets nowej czcionki Berylium, przycisku "zasady gry". Przeskalowanie przycisków i dodanie do nich nowej czcionki.

--- menu.py
+++ menu.py
@@ -1,4 +1,10 @@
-import pygame, sys
+#dodać przycisk zasady gry, który przekierowuje do strony internetowej z zasadami gry OK
+#dopasować przyciski z menu głównego wraz z czcionką, rozmiarem i kolorystyką OK
+#https://www.fontsquirrel.com/
+
+import pygame
+import sys
+import webbrowser
 from button import Button
 import os
 
@@ -7,19 +13,22 @@ pygame.init()
 os.environ['SOL_VIDEO_CENTERED'] = '1'
 info = pygame.display.Info()
 
-#Rozmiary okna gry, oryginalnie otwiera się w fullscreenie:
+# Rozmiary okna gry, oryginalnie otwiera się w fullscreenie:
 SCREEN_WIDTH = info.current_w
 SCREEN_HEIGHT = info.current_h
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-#Nazwa okna gry:
+# Nazwa okna gry:
 pygame.display.set_caption("Sen")
 
-#Tło gry:
+# Tło gry:
 BG = pygame.image.load("assets/menu.png")
 BG_SCALED = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-def get_font(size):  #Zwraca czcionkę o określonym rozmiarze
+def get_font(size):  # Zwraca czcionkę o określonym rozmiarze
+    return pygame.font.Font("assets/Berylium/Berylium.ttf", size)
+
+def get_title(size):
     return pygame.font.Font("assets/ringbearer/RINGM___.TTF", size)
 
 def main_menu():
@@ -28,19 +37,21 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(170).render("Sen", True, "#8a6539")
-        MENU_RECT = MENU_TEXT.get_rect(center=(200, 100))
+        MENU_TEXT = get_title(170).render("Sen", True, "#8a6539")
+        MENU_RECT = MENU_TEXT.get_rect(center=(200, 120))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 250),
-                             text_input="Graj", font=get_font(50), base_color="#d7fcd4", hovering_color="White", scale=2)
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 400),
-                                text_input="Ustawienia", font=get_font(50), base_color="#d7fcd4", hovering_color="White", scale=2)
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 550),
-                             text_input="Wyjscie", font=get_font(50), base_color="#d7fcd4", hovering_color="White", scale=2)
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 290),
+                             text_input="Graj", font=get_font(43), base_color="#674a29", hovering_color="White", scale=2)
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 440),
+                                text_input="Ustawienia", font=get_font(43), base_color="#674a29", hovering_color="White", scale=2)
+        RULES_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 590),
+                              text_input="Zasady Gry", font=get_font(43), base_color="#674a29", hovering_color="White", scale=2)
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 740),
+                             text_input="Wyjście", font=get_font(43), base_color="#674a29", hovering_color="White", scale=2)
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
-        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, RULES_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
 
@@ -53,13 +64,18 @@ def main_menu():
                     play()
                 if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
                     options()
+                if RULES_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    open_rules()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
 
         pygame.display.update()
 
-
+def open_rules():
+    # Funkcja otwierająca stronę z zasadami gry
+    url = "https://gry.nk.com.pl/wp-content/uploads/2022/03/SEN-instrukcja.pdf"
+    webbrowser.open(url)
 
 def play():
     while True:
@@ -86,7 +102,6 @@ def play():
                     main_menu()
 
         pygame.display.update()
-
 
 def options():
     while True:
@@ -115,3 +130,4 @@ def options():
         pygame.display.update()
 
 main_menu()
+
