f0967454479d638f5090694b88976991eb25459e
brixen05 <majka.skora@gmail.com>
2025-01-05 23:10:23 +0100

    Zrobienie funkcji fullscreen oraz dodanie skrótu klawiszowego f11, który przełącza tryb okna. Usunięcie przycisku resolution i dodanie autorów.

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
-#ustawić tło do ustawień, dodać w ustawieniach napisy dźwięk, rozdzielczość, ekran OK
-#ogarnąć działanie ekranu (fullscreen, windowed) OK
+#Zrobienie funkcji fullscreen oraz dodanie skrótu klawiszowego f11, który przełącza tryb okna. OK
+#Usunięcie resolution i dodanie autorów OK
 
 import pygame
 import sys
@@ -26,12 +26,26 @@ BG = pygame.image.load("assets/menu.png")
 BG_SCALED = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
 OP = pygame.image.load("assets/menu_rozmazane.png")
 OP_SCALED = pygame.transform.scale(OP, (SCREEN_WIDTH, SCREEN_HEIGHT))
+TABLE = pygame.image.load("assets/stół.png")
+TABLE_SCALED = pygame.transform.scale(TABLE, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 def get_font(size):  # Zwraca czcionkę o określonym rozmiarze
     return pygame.font.Font("assets/Berylium/Berylium.ttf", size)
 
+
 def get_title(size):
     return pygame.font.Font("assets/ringbearer/RINGM___.TTF", size)
+
+
+def toggle_fullscreen():
+    global fullscreen, SCREEN
+    fullscreen = not fullscreen
+    if fullscreen:
+        SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    else:
+        SCREEN = pygame.display.set_mode((SCREEN_WIDTH - 10, SCREEN_HEIGHT - 50))
+
 
 def main_menu():
     while True:
@@ -43,13 +57,17 @@ def main_menu():
         MENU_RECT = MENU_TEXT.get_rect(center=(200, 120))
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 290),
-                             text_input="Graj", font=get_font(43), base_color="#674a29", hovering_color="White", scale=2)
+                             text_input="Graj", font=get_font(43), base_color="#674a29", hovering_color="White",
+                             scale=2)
         OPTIONS_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 440),
-                                text_input="Ustawienia", font=get_font(43), base_color="#674a29", hovering_color="White", scale=2)
+                                text_input="Ustawienia", font=get_font(43), base_color="#674a29",
+                                hovering_color="White", scale=2)
         RULES_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 590),
-                              text_input="Zasady Gry", font=get_font(43), base_color="#674a29", hovering_color="White", scale=2)
+                              text_input="Zasady Gry", font=get_font(43), base_color="#674a29", hovering_color="White",
+                              scale=2)
         QUIT_BUTTON = Button(image=pygame.image.load("assets/przycisk.png"), pos=(200, 740),
-                             text_input="Wyjście", font=get_font(43), base_color="#674a29", hovering_color="White", scale=2)
+                             text_input="Wyjście", font=get_font(43), base_color="#674a29", hovering_color="White",
+                             scale=2)
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
@@ -71,13 +89,18 @@ def main_menu():
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F11:  # F11 to klawisz skrótu
+                    toggle_fullscreen()
 
         pygame.display.update()
+
 
 def open_rules():
     # Funkcja otwierająca stronę z zasadami gry
     url = "https://gry.nk.com.pl/wp-content/uploads/2022/03/SEN-instrukcja.pdf"
     webbrowser.open(url)
+
 
 def play():
     while True:
@@ -102,8 +125,60 @@ def play():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
                     main_menu()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F11:  # F11 to klawisz skrótu
+                    toggle_fullscreen()
 
         pygame.display.update()
+
+
+def show_authors():
+    while True:
+        AUTHOR_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.blit(TABLE_SCALED, (0, 0))
+
+        AUTHOR_TEXT = get_font(50).render("Autorzy gry:", True, "White")
+        AUTHOR_RECT = AUTHOR_TEXT.get_rect(center=(780, 150))
+        SCREEN.blit(AUTHOR_TEXT, AUTHOR_RECT)
+
+        # Lista autorów gry
+        AUTHOR_NAMES = [
+            "Wojtek Cieślik - Gameplay",
+            "Hania Szlęk - Grafika",
+            "Maja Skóra - Menu gry",
+            "Olga Russell - Boty",
+            "Joanna Frączyk - Core logic",
+            "Magdalena Majda, Wiktoria Szczepaniak - Warianty gry"
+        ]
+
+        y_offset = 250
+        for name in AUTHOR_NAMES:
+            author_text = get_font(40).render(name, True, "White")
+            author_rect = author_text.get_rect(center=(780, y_offset))
+            SCREEN.blit(author_text, author_rect)
+            y_offset += 50
+
+        AUTHOR_BACK = Button(image=None, pos=(780, 650),
+                             text_input="Powrót", font=get_font(43), base_color="#674a29", hovering_color="White",
+                             scale=1)
+
+        AUTHOR_BACK.changeColor(AUTHOR_MOUSE_POS)
+        AUTHOR_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if AUTHOR_BACK.checkForInput(AUTHOR_MOUSE_POS):
+                    options()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F11:  # F11 to klawisz skrótu
+                    toggle_fullscreen()
+
+        pygame.display.update()
+
 
 def options():
     global SCREEN, fullscreen  # Zmienna globalna dla okna gry i trybu fullscreen
@@ -114,15 +189,12 @@ def options():
 
         # Napisy na ustawienia dźwięku, rozdzielczości, ekranu
         OPTIONS_DZW = get_font(43).render("Dźwięk", True, "#674a29")
-        OPTIONS_ROZ = get_font(43).render("Rozdzielczość", True, "#674a29")
         OPTIONS_EKR = get_font(43).render("Ekran", True, "#674a29")
 
         OPTIONS_RECT_D = OPTIONS_DZW.get_rect(center=(620, 200))
-        OPTIONS_RECT_R = OPTIONS_ROZ.get_rect(center=(620, 350))
         OPTIONS_RECT_E = OPTIONS_EKR.get_rect(center=(620, 500))
 
         SCREEN.blit(OPTIONS_DZW, OPTIONS_RECT_D)
-        SCREEN.blit(OPTIONS_ROZ, OPTIONS_RECT_R)
         SCREEN.blit(OPTIONS_EKR, OPTIONS_RECT_E)
 
         # Napisy z informacją o trybie ekranu:
@@ -135,15 +207,24 @@ def options():
 
         # Przycisk back:
         OPTIONS_BACK = Button(image=None, pos=(620, 650),
-                              text_input="Powrót", font=get_font(43), base_color="#674a29", hovering_color="White", scale=1)
+                              text_input="Powrót", font=get_font(43), base_color="#674a29", hovering_color="White",
+                              scale=1)
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
 
+        # Przycisk autorzy:
+        OPTIONS_AUT = Button(image=None, pos=(620, 350), text_input="Autorzy", font=get_font(43), base_color="#674a29",
+                             hovering_color="White", scale=1)
+        OPTIONS_AUT.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_AUT.update(SCREEN)
+
         # Przyciski strzałek do zmiany trybu ekranu:
         OPTIONS_ARROW_L = Button(image=pygame.image.load("assets/strzałka2.png"), pos=(750, 500),
-                                  text_input=None, font=get_font(43), base_color="#674a29", hovering_color="White", scale=0.35)
+                                 text_input=None, font=get_font(43), base_color="#674a29", hovering_color="White",
+                                 scale=0.35)
         OPTIONS_ARROW_R = Button(image=pygame.image.load("assets/strzałka.png"), pos=(1000, 500),
-                                  text_input=None, font=get_font(43), base_color="#674a29", hovering_color="White", scale=0.35)
+                                 text_input=None, font=get_font(43), base_color="#674a29", hovering_color="White",
+                                 scale=0.35)
         OPTIONS_ARROW_L.update(SCREEN)
         OPTIONS_ARROW_R.update(SCREEN)
 
@@ -156,13 +237,15 @@ def options():
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
                     main_menu()
                 if OPTIONS_ARROW_L.checkForInput(OPTIONS_MOUSE_POS) or OPTIONS_ARROW_R.checkForInput(OPTIONS_MOUSE_POS):
-                    fullscreen = not fullscreen
-                    if fullscreen:
-                        SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-                    else:
-                        SCREEN = pygame.display.set_mode((SCREEN_WIDTH - 10, SCREEN_HEIGHT - 50)) # Okno mniejsze
+                    toggle_fullscreen()
+                if OPTIONS_AUT.checkForInput(OPTIONS_MOUSE_POS):  # Przekierowanie do okna autorów
+                    show_authors()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F11:  # F11 to klawisz skrótu
+                    toggle_fullscreen()
 
         pygame.display.update()
+
 
 main_menu()
 
