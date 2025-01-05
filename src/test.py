df67820723276b7a5c42a7586970a9ea92f0af55
wojtekc135 <wojtekc135@gmail.com>
2025-01-05 07:33:18 +0100

    Refactor reorganize classes and methods between files

--- src/test.py
+++ src/test.py
@@ -2,128 +2,28 @@ import pygame
 from game_render import GameRenderer
 from input_handler import InputHandler
 from src.round import Round
-from player import Player
 from card import Card
 import os
 from utils import load_assets, scale_assets, get_card_size
 from random import choice
 
-
-def create_example_state(screen, assets, state):
-    id=0
-    for hand in ["hand1", "hand2", "hand3", "hand4"]:
-        for i in range(4):
-            state[hand].append(
-                Card(screen, assets["cards"][i], assets["card_back"], False, False, hand, i, False, False, card_size,id))
-            id+=1
-    state["face_up_pile"].append(Card(screen, assets["cards"][0], assets["card_back"], True, True, "face_up_pile", 0, False, False, card_size,id))
-    id+=1
-    state["face_up_pile"].append(Card(screen, assets["cards"][1], assets["card_back"], True, True, "face_up_pile", 1, False, False, card_size,id))
-    id+=1
-    state["face_down_pile"].append(Card(screen, assets["cards"][0], assets["card_back"], False, False, "face_down_pile", 0,
-                                    False, False, card_size,id))
-    id+=1
-    state["face_down_pile"].append(Card(screen, assets["cards"][1], assets["card_back"], False, False, "face_down_pile", 1,
-                                    False, False, card_size, id))
-    for localisation in state:
-        for card in state[localisation]:
-            card.update_position() # zainicjalizowanie kart bardzo  ważne
-    return state
-
-
-def current_player(nr):
-    if game_round.round_number % 4 == 1:
-        return player1
-    if game_round.round_number % 4 == 2:
-        return player2
-    if game_round.round_number % 4 == 3:
-        return player3
-    if game_round.round_number % 4 == 0:
-        return player4
-
-
-def show_2_cards(hand, game_renderer, cur_player, state, action_text, game_round,
-                 isHuman):
-    game_renderer.draw_state(cur_player, state, action_text)
-    while game_round.count_known_for_player(hand) < 2:
-        if isHuman == "human":
-            picked_card = InputHandler.choose_from(hand)
-            if not picked_card.known_for_player:
-                picked_card.show_front = True
-                picked_card.known_for_player = True
-                picked_card.selected_info = "Niewidoczna"
-
-        # dla bota
-        if isHuman == "bot":
-            picked_card = choice(hand)
-            if not picked_card.known_for_player:
-                picked_card.known_for_player = True
-                picked_card.show_front = False
-                picked_card.highlighted = True
-                game_renderer.draw_state(cur_player, state, action_text)
-                picked_card.highlighted = False
-        game_renderer.draw_state(cur_player, state, action_text)
-
-
-def wes_karte_z_dowolnego_stosu_ze_spodu(state):
-    # Żeby była informacja wybrano pod stackiem proponuje zrobić nową metode (ciezko pobrac lokalizacje, trzeba z card z update wziac)
-    # lub ustawiać tą informacje na karte na samej górze stacku i potem ją zdejmować, ponieważ draw_state wyswietla tylko ostanią karte z stacka
-    # wybranie karty z dowolnego
-    game_renderer.draw_state(cur_player, state, "Wybierz stos")
-    stack_type = player1.choose_stack_type(state)
-    stack_index_to_choose = -1 # spód karty
-    # stack_index_to_choose = -1 góra
-    card_from_stack =  player1.choose_card_from_stack(state, stack_type, stack_index_to_choose)
-
-    game_renderer.draw_state(cur_player, state, " Wybierz karte z ręki")
-    card2 = player1.choose_card_from_hand(state, "hand1")
-    card2.selected_info = "wybrano"
-
-    game_renderer.draw_state(cur_player, state, "Zamienianie miejscami")
-    pygame.time.wait(500)
-    state, card1, card2 = player1.swap_card(state, card_from_stack,
-                                               card2)
-    card2.selected_info = False
-    if cur_player.isHuman == "human":
-        card1.selected_info = "niewidoczna"
-        card1.show_front = True
-    else:
-        card1.selected_info = False
-    game_round.print_state(state)
-    game_renderer.draw_state(cur_player, state, "Zamieniono miejscami")
-
-
 pygame.init()
 
+game_round = Round(1, "human", 1)
 screen_info = pygame.display.Info()
 screen_width = screen_info.current_w
 screen_height = screen_info.current_h
 screen = pygame.display.set_mode((screen_width, screen_height))
 font = pygame.font.SysFont("arial", 24)
-
 card_size = get_card_size(screen_height)
 assets = load_assets(os.path.join(os.pardir, "assets"), "karta", "stół", "rewers")
 assets = scale_assets(assets, card_size, (screen_width, screen_height))
+state = game_round.create_example_state(screen, assets, card_size)
+game_round.debug(state)
 
-state = {
-    "hand1": [],
-    "hand2": [],
-    "hand3": [],
-    "hand4": [],
-    "face_down_pile": [],
-    "face_up_pile": []
-}
-state = create_example_state(screen, assets, state)
 game_renderer = GameRenderer(screen, assets, font)
-game_round = Round(1)
-player1 = Player("human", "1")
-player2 = Player("bot", "2")
-player3 = Player("bot", "3")
-player4 = Player("bot", "4")
-game_round.print_state(state)
 clock = pygame.time.Clock()
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -132,19 +32,27 @@ while running:
             if event.key == pygame.K_q:
                 running = False
 
-    cur_player = current_player(game_round.round_number)
-    cur_hand = "hand" + str(cur_player.player_number)
+    cur_hand = "hand" + str(game_round.player_number)
 
     if game_round.round_number <= 4:
-        if cur_player.isHuman == "bot":
+        if game_round.player_type == "bot":
             action_text = "Boty podglądają karty"
         else:
             action_text = "Podglądnij 2 karty"
-        show_2_cards(state[cur_hand], game_renderer, cur_player, state, action_text, game_round, cur_player.isHuman)
+        game_round.show_2_cards(state[cur_hand], game_renderer, game_round, state, action_text)
 
     elif game_round.round_number > 4:
-        wes_karte_z_dowolnego_stosu_ze_spodu(state)
+        game_round.wes_karte_z_dowolnego_stosu_ze_spodu(state, game_round, game_renderer)
 
     game_round.round_number += 1
+    if game_round.round_number % 4 == 1:
+        game_round.player_type = "human"
+    else:
+        game_round.player_type = "bot"
+    if game_round.round_number % 4 == 0:
+        game_round.player_number = 4
+    else:
+        game_round.player_number = game_round.round_number % 4
+
     pygame.time.wait(200)
 pygame.quit()
