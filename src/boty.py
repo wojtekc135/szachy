ee4e96a32da942c31fb9a187ae105a815ac798e8
HSP <98179060+HighlySensitivePsychopath@users.noreply.github.com>
2025-01-11 12:45:48 +0100

    bot behaviour at the start of the game

--- src/boty.py
+++ src/boty.py
@@ -4,3 +4,6 @@ class Bot():
     def __init__(self):
         self.name=name
         self.dream=[None]*4
+    def start_game(selfself, initial_card):
+        self.dream=initial_cards
+        self.known_cards=random.sample(range(4), 2)
